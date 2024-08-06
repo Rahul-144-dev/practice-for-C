b2104c6e8e633e11cc8ef8256adaebc0b05b5f6d
RAHUL144 <117508363+rahulsaha1045451444@users.noreply.github.com>
2024-08-06 23:28:37 +0530

    26

--- 01_Basic Declarations and Expressions/026.c
+++ 01_Basic Declarations and Expressions/026.c
@@ -1,12 +1,16 @@
+//* Write a C program that prints all even numbers between 1 and 50 (inclusive)
 #include <stdio.h>
 #include <math.h>
 int main()
 {
-   int n;
+   int n, i = 1;
    printf("enter number : ");
    scanf("%d", &n);
-   for (int i = 0; i <= n; i++)
+   while (i <= n)
    {
+      if (i % 2 == 0)
+         printf("%d ", i);
+      i++;
    }
    return 0;
 }
