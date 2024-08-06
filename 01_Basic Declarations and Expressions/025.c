5b9d715b3a44c5e539ccb3e1852c88763afae632
RAHUL144 <117508363+rahulsaha1045451444@users.noreply.github.com>
2024-08-06 23:23:52 +0530

    switch

--- 01_Basic Declarations and Expressions/025.c
+++ 01_Basic Declarations and Expressions/025.c
@@ -3,9 +3,52 @@
 #include <math.h>
 int main()
 {
-   int n;
-   printf("enter number : ");
-   scanf("%d", &n);
-
+   int month_no;
+   do
+   {
+      printf("enter month number : ");
+      scanf("%d", &month_no);
+      switch (month_no)
+      {
+      case 1:
+         printf("january\n");
+         break;
+      case 2:
+         printf("february\n");
+         break;
+      case 3:
+         printf("march\n");
+         break;
+      case 4:
+         printf("april\n");
+         break;
+      case 5:
+         printf("may\n");
+         break;
+      case 6:
+         printf("june\n");
+         break;
+      case 7:
+         printf("july\n");
+         break;
+      case 8:
+         printf("august\n");
+         break;
+      case 9:
+         printf("september\n");
+         break;
+      case 10:
+         printf("october\n");
+         break;
+      case 11:
+         printf("november\n");
+         break;
+      case 12:
+         printf("december\n");
+         break;
+      }
+      if (month_no < 1 || month_no > 12)
+         printf("entered a vaild number 1-12");
+   } while (month_no < 1 || month_no > 12);
    return 0;
 }
